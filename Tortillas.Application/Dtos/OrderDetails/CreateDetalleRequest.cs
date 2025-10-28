a5c681fce2d0e53ec8352ad8891ad14253867870
Alison Montufar <alisonmontufar14@gmail.com>
2025-10-28 09:56:29 -0600

    Sprint3(Pedido):Creacion de request para pedidos

---
+++ Tortillas.Application/Dtos/OrderDetails/CreateDetalleRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortillas.Application.Dtos.OrderDetails
+{
+    public class CreateDetalleRequest
+    {
+        public int FkDireccion { get; set; }
+        public string ProductoNombre { get; set; } = string.Empty;
+        public decimal Cantidad { get; set; }
+        public string? EstatusNombre { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string? NombreSucursal { get; set; }
+    }
+
+}
---
+++ Tortillas.Application/Dtos/Pay/PagoDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortillas.Application.Dtos.Pay
+{
+    public class PagoDto
+    {
+        public int Id { get; set; }
+        public int FkPedido { get; set; }
+        public string MetodoPago { get; set; } = string.Empty;
+        public string? NumeroEnmascarado { get; set; }
+        public string? MarcaTarjeta { get; set; }
+        public byte? ExpMes { get; set; }
+        public short? ExpAnio { get; set; }
+        public string? NombreTitular { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
